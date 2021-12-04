15cf4184dadd0752a31aa22a95f5b29113fe67a7
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-04 16:09:10 +0600

    mui menu

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Description from '../Description/Description';
 import DishMenu from '../DishMenu/DishMenu';
+import DishMenu2 from '../DishMenu/DishMenu2';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MainMenu from '../MainMenu/MainMenu';
@@ -10,7 +11,7 @@ const Home = () => {
         <div>
             <MainMenu />
             <Header />
-            <DishMenu />
+            <DishMenu2 />
             <Description />
             <Footer />
         </div>
