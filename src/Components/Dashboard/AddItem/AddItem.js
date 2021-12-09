a9df252b222a81fe062f355339dcc21283e43257
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-09 23:58:15 +0600

    update form

--- src/Components/Dashboard/AddItem/AddItem.js
+++ src/Components/Dashboard/AddItem/AddItem.js
@@ -17,25 +17,25 @@ const AddItem = () => {
           <div className="col-md-8">
               <form class="row g-3">
                   <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Item Name</label>
-                    <input type="email" class="form-control" id="inputEmail4" />
+                    <label for="name" class="form-label">Item Name</label>
+                    <input type="text" class="form-control" id="name" />
                   </div>
                   <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Item Price</label>
-                    <input type="number" class="form-control" id="inputPassword4" />
+                    <label for="price" class="form-label">Item Price</label>
+                    <input type="number" class="form-control" id="price" />
                   </div>
                   <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" /> 
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" id="description" rows="3"></textarea> 
                   </div>
-                  
+
                   <div class="mb-3 col-md-6">
-                    <label for="formFile" class="form-label">Default file input example</label><br/>
+                    <label for="formFile" class="form-label">Item Image</label><br/>
                     <input type="file" id="formFile" />
                   </div>
 
                   <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                   </div>
               </form>
           </div>
