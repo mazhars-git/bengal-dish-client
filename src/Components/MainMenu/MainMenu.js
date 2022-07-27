6026a226c4119eefc1a3c3676468a6befbc18f25
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-27 20:24:40 +0600

    shopping cart icon added

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../../images/logo2.png';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const MainMenu = () => {
     return (
@@ -15,8 +16,10 @@ const MainMenu = () => {
                         style={{ maxHeight: '100px'}}
                         navbarScroll
                     >
+                        <Nav.Link href="#">
+                            <ShoppingCartOutlinedIcon />
+                        </Nav.Link>
                         <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/menu">Menu</Nav.Link>
                         <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
