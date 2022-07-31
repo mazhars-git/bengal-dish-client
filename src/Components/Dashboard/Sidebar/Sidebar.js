0c0aa9a8244c2a981a7927b6cad0b5980681a9e5
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-31 23:45:22 +0600

    sidebar

--- src/Components/Dashboard/Sidebar/Sidebar.js
+++ src/Components/Dashboard/Sidebar/Sidebar.js
@@ -3,19 +3,20 @@ import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListIcon from '@material-ui/icons/List';
 import Typography from '@material-ui/core/Typography';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { Link } from 'react-router-dom';
 import AddIcon from '@material-ui/icons/Add';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import '../../Css/Sidebar.css';
 
 const useStyles = makeStyles({
     root: {
       width: 230,
     },
   });
+
 
 const Sidebar = () => {
 
@@ -46,7 +47,7 @@ const Sidebar = () => {
                 <MenuItem>
                     <Link to="/">
                         <ExitToAppIcon color="secondary" fontSize="medium" />
-                        <Typography variant="inherit">
+                        <Typography paddingLeft="10px" variant="inherit">
                             Back
                         </Typography>
                     </Link>
