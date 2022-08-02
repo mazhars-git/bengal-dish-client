42c3cb488b5208372bbe7035819eb7f95c367117
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-02 23:21:17 +0600

    update

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const CheckOut = () => {
     return (
-        <div className="container">
-            <h1>CheckOut Page</h1>
-        </div>
+        <section>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Light BreakFast</h1>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown</p>
+                    </div>
+                    <div className="col-md-6">
+                        <img src="../../images/des-1.png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     );
 };
 
