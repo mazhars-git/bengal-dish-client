f6695332f24d4918d17b51c9b6979f2fa2a7c551
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-18 09:15:31 +0600

    add item form update

--- src/Components/Dashboard/AddItem/AddItem.js
+++ src/Components/Dashboard/AddItem/AddItem.js
@@ -10,32 +10,46 @@ const AddItem = () => {
     <section className="dashboard">
       <div className="container py-5">
         <div className="row">
+  {/* --------------------------sidebar------------------- */}
+
           <div className="col-md-3">
               <Sidebar />
           </div>
-          
+    
+    {/* ----------------------add item form ---------------------------- */}
+
           <div className="col-md-9 addForm">
-              <form class="row g-3">
-                  <div class="col-md-6">
-                    <label for="name" class="form-label">Item Name</label>
-                    <input type="text" class="form-control" id="name" />
+              <form className="row g-3">
+                <div className="col-md-4">
+                    <label for="name" className="form-label">Item Name</label>
+                    <input type="text" className="form-control" id="name" />
                   </div>
-                  <div class="col-md-6">
-                    <label for="price" class="form-label">Item Price</label>
-                    <input type="number" class="form-control" id="price" />
+                  <div className="col-md-4">
+                    <label for="price" className="form-label">Item Price</label>
+                    <input type="number" className="form-control" id="price" />
                   </div>
-                  <div class="col-12">
-                    <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control" id="description" rows="3"></textarea> 
+                  <div className="col-md-4">
+                    <label for="category" className="form-label">Category</label>
+                      <select className="form-control" id="category">
+                          <option selected="true" disabled="disabled">Select Category</option>
+                          <option>Breakfast</option>
+                          <option>Lunch</option>
+                          <option>Dinner</option>
+                      </select>
+                  </div>
+                  
+                  <div className="col-12">
+                    <label for="description" className="form-label mt-2">Description</label>
+                    <textarea className="form-control" id="description" rows="3"></textarea> 
                   </div>
 
-                  <div class="mb-3 col-md-6">
-                    <label for="formFile" class="form-label">Item Image</label><br/>
+                  <div className="mb-3 col-md-6">
+                    <label for="formFile" className="form-label mt-2">Item Image</label><br/>
                     <input type="file" id="formFile" />
                   </div>
 
-                  <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                  <div className="col-12">
+                    <button type="submit" className="btn btn-primary">Submit</button>
                   </div>
               </form>
           </div>
