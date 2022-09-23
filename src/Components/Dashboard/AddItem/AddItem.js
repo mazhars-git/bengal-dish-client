89e5c1bc68612bb932bf2a717fc2c1a061b9df9f
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-23 10:28:08 +0600

    form update

--- src/Components/Dashboard/AddItem/AddItem.js
+++ src/Components/Dashboard/AddItem/AddItem.js
@@ -7,7 +7,15 @@ const AddItem = () => {
 
   const handleSubmit= (e) =>{
     e.preventDefault();
-    console.log('submit clicked')
+    const item = e.target.itemName.value;
+    const price = e.target.price.value;
+    const itemCategory = e.target.category.value;
+    const itemDescription = e.target.description.value;
+    const image = e.target.itemImage.value;
+
+    const mealItem = {item, price, itemCategory, itemDescription, image};
+
+    console.log(mealItem)
   }
   
 
@@ -26,16 +34,16 @@ const AddItem = () => {
           <div className="col-md-9 addForm">
               <form className="row g-3" onSubmit={handleSubmit}>
                 <div className="col-md-4">
-                    <label for="name" className="form-label">Item Name</label>
-                    <input type="text" className="form-control" id="name" />
+                    <label for="itemName" className="form-label">Item Name</label>
+                    <input type="text" className="form-control" id="itemName" required />
                   </div>
                   <div className="col-md-4">
                     <label for="price" className="form-label">Item Price</label>
-                    <input type="number" className="form-control" id="price" />
+                    <input type="number" className="form-control" id="price" required />
                   </div>
                   <div className="col-md-4">
                     <label for="category" className="form-label">Category</label>
-                      <select className="form-control" id="category">
+                      <select className="form-control" id="category" required>
                           <option selected="true" disabled="disabled">Select Category</option>
                           <option>Breakfast</option>
                           <option>Lunch</option>
@@ -45,12 +53,12 @@ const AddItem = () => {
                   
                   <div className="col-12">
                     <label for="description" className="form-label mt-2">Description</label>
-                    <textarea className="form-control" id="description" rows="3"></textarea> 
+                    <textarea className="form-control" id="description" rows="3" required></textarea> 
                   </div>
 
                   <div className="mb-3 col-md-6">
-                    <label for="formFile" className="form-label mt-2">Item Image</label><br/>
-                    <input type="file" id="formFile" />
+                    <label for="itemImage" className="form-label mt-2">Item Image</label><br/>
+                    <input type="file" id="itemImage" required />
                   </div>
 
                   <div className="col-12 d-flex justify-content-end">
