3b244272cefa84595a5124ed5a5ee79214d990cd
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-10-19 23:13:08 +0600

    checkout page done

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -3,28 +3,29 @@ import '../Css/CheckOut.css';
 import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom';
 
-const CheckOut = () => {
+const CheckOut = (props) => {
     const {itemId} = useParams();
-
-    const {item, setItem} = useState({});
+    const [booking, setBooking] = useState({});
 
     useEffect(() =>{
         fetch(`http://localhost:5000/item/${itemId}`)
         .then(res => res.json())
-        .then(data => setItem(data));
+        .then(data => setBooking(data))
     },[])
 
-    console.log(item);
+    console.log(booking);
+
+
     return (
         <section>
             <div className="container py-5">
                 <div className="row">
                     <div className="col-md-6">
-                        <h1>Light BreakFast: </h1>
+                        <h1>{booking.item} </h1>
                         <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown</p>
                     </div>
                     <div className="col-md-6">
-                        <img width='400' height= '200' alt='Item_Image' src="#"></img>
+                        <img width='400' alt='Item_Image' src={booking.image}></img>
                     </div>
                     <button className="btn-brand">
                         Order Now
