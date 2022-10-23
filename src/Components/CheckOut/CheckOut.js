1acbea456a04acb77f8aba2c0c24156561bff1cf
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-10-23 23:32:38 +0600

    update

---
+++ src/Components/CheckOut/CheckOut.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CheckOut = () => {
+    return (
+        <section>
+            <div className="container">
+                <div classNam="row">
+                    <div className="col-md-6">
+                        <h2>Address and Payment</h2>
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Your Cart</h2>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+export default CheckOut;
