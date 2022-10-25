9c4b6cfa7a2f93fe260ef1fa2c0fe7b6ddce3046
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-10-25 10:43:30 +0600

    index uyp

--- src/Components/Dashboard/ManagePd/ManagePd.js
+++ src/Components/Dashboard/ManagePd/ManagePd.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
+import UseItems from './../../UseItems/UseItems';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const ManagePd = () => {
+    const [itemsData] = UseItems();
     return (
-        <div className="dashboard">
+        <section className="dashboard">
             <div className="container">
                 <div className="row pt-5">
                     <div className="col-md-3">
@@ -16,31 +20,25 @@ const ManagePd = () => {
                             <table class="table table-light">
                                 <thead>
                                     <tr>
-                                    <th scope="col">Sl</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Address</th>
-                                    <th scope="col">Gmail</th>
+                                    <th scope="col">Sl No</th>
+                                    <th scope="col">Item Name</th>
+                                    <th scope="col">Item Price</th>
+                                    <th scope="col">Action Btn</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                    </tr>
+                                    
+                                    {
+                                        itemsData.map((items, index) => 
+                                        <tr key={items._id}>
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{items.item}</td>
+                                            <td>{items.price}</td>
+                                            <td><FontAwesomeIcon icon={faFacebook}/></td>
+                                        </tr>
+                                        )
+                                    }
+                                    
                                 </tbody>
                             </table>
                        </div>
@@ -48,7 +46,7 @@ const ManagePd = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
